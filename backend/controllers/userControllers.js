def30fceafc75960bcdd7d5e58e034f5110d9d41
Darrel Holder <darrel.holder@gmail.com>
2022-11-09 17:29:23 -0500

    added middleware functionality

--- backend/controllers/userControllers.js
+++ backend/controllers/userControllers.js
@@ -1,10 +1,24 @@
-const registerUser = (req, res) =>{
-    res.send('Register Route')
-}
 
-const loginUser = (req, res) => {
-    res.send('Login Route')
+/*
+@desc Register a new user
+@route /api/users
+@access Public
+*/
+const registerUser = asyncHandler(async (req, res) => {
+    const {name, email, password} = req.body
+
+//validation
+if(!name || !email || !password){
+   res.status(400)
+   throw new Error('Error on RegisterUser')
+   
 }
+    res.send('Register Route')
+})
+
+const loginUser = asyncHandler(async (req, res) => {
+    res.send('Login Route')
+})
 
 module.exports = {
     registerUser,
--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,16 @@
 const express = require('express')
 const dotenv = require('dotenv').config()
 const PORT = process.env.PORT || 8000
+const {errorHandler} = require('./middleware/errorMiddleware')
 const app = express()
+
+app.use(express.json())
+app.use(express.urlencoded({extended: false}))
 
 app.get('/', (req, res) => {
     res.status(201).json({message: 'Welcome to Support Ticketing system' })
 })
 app.use('/api/users', require('./routes/userRoutes'))
+
+app.use(errorHandler)
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`))
