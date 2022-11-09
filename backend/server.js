b11e4379ad0c297eb5e018d829516a6631bbc16e
Darrel Holder <darrel.holder@gmail.com>
2022-11-09 18:35:00 -0500

    added colors and connecting to db

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,11 @@ const dotenv = require('dotenv').config()
 const PORT = process.env.PORT || 8000
 const {errorHandler} = require('./middleware/errorMiddleware')
 const app = express()
+const connectDB = require('./config/db')
+const colors = require('colors')
+
+//Connect to database
+connectDB()
 
 app.use(express.json())
 app.use(express.urlencoded({extended: false}))
