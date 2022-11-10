e470dccd313649e2ea13807ab6fffdd43f39c390
Darrel Holder <darrel.holder@gmail.com>
2022-11-09 22:24:35 -0500

    ability to register new user

--- backend/controllers/userControllers.js
+++ backend/controllers/userControllers.js
@@ -1,4 +1,7 @@
 const asyncHandler = require('express-async-handler')
+const bcrypt = require('bcryptjs')
+const User = require('../models/userModels')
+const JWT = require('jsonwebtoken')
 /*
 @desc Register a new user
 @route /api/users
@@ -13,12 +16,60 @@ if(!name || !email || !password){
    throw new Error('Error on RegisterUser')
    
 }
-    res.send('Register Route')
+
+// Find if user exists
+
+const userExists = await User.findOne({email})
+if(userExists){
+    res.status(400)
+    throw new Error('User already exists')
+}
+// hash password
+const salt = await bcrypt.genSalt(10)
+const hashedPassword = await bcrypt.hash(password, salt)
+// Create user
+const user = await User.create({
+    name, email, password: hashedPassword
 })
 
-const loginUser = asyncHandler(async (req, res) => {
-    res.send('Login Route')
+if(user) {
+    res.status(201).json({
+        _id: user._id,
+        name: user.name,
+        email: user.email,
+        token: generateToken(user._id)
+    })
+} else {
+    res.status(400)
+    throw new error('Invalid user data')
+}
 })
+
+// Login User
+
+const loginUser = asyncHandler(async (req, res) => {
+const {email, password} = req.body
+const user = await User.findOne({email})
+// Check that passwords match
+    if(user && (await bcrypt.compare(password, user.password))) {
+        res.status(200).json({
+            _id: user._id,
+            name: user.name,
+            email: user.email,
+            token: generateToken(user._id)
+        })
+    } else {
+        res.status(401)
+        throw new Error('Invalid credentials')
+    }
+})
+
+// Generate Token
+const generateToken = (id) => {
+    return JWT.sign({id},process.env.JWT_SECRET, {
+        expiresIn: '30d',
+    })
+}
 
 module.exports = {
     registerUser,
