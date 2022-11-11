e4eaa289bea71f19284b0b76b01cafa3a618cbc7
Darrel Holder <darrel.holder@gmail.com>
2022-11-11 16:31:19 -0500

    added Auth, E131 done

--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -1,7 +1,8 @@
 import { useState } from "react";
-import { FaUser } from "react-icons/fa";
+import { FaUser } from "react-icons/fa"
 import {toast} from "react-toastify"
-
+import {useSelector, useDispatch} from 'react-redux'
+import { register } from "../features/auth/authSlice";
 
 
 function Register() {
@@ -14,6 +15,10 @@ function Register() {
 
 const {name, email, password, password2} = formData
 
+const dispatch = useDispatch()
+
+const {user, isLoading, isSuccess, message} = useSelector(state => state.auth)
+
 const onChange = (e) => {
   setFormData((prevState)=>({ ...prevState, [e.target.id]: e.target.value,}))
 }
@@ -23,6 +28,11 @@ const onSubmit = (e) => {
 
   if(password !== password2){
     toast.error("Passwords don't match")
+  } else {
+    const userData = {
+      name, email, password
+    }
+    dispatch(register(userData))
   }
 }
 
