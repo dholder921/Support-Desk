09427fd13d122b9a6fec5f531d9598323afa6a45
Darrel Holder <darrel.holder@gmail.com>
2022-11-12 21:48:41 -0500

    Added controller E135

--- backend/server.js
+++ backend/server.js
@@ -16,6 +16,7 @@ app.get('/', (req, res) => {
     res.status(201).json({message: 'Welcome to Support Ticketing system' })
 })
 app.use('/api/users', require('./routes/userRoutes'))
+app.use('/api/tickets', require('./routes/ticketRoutes'))
 
 app.use(errorHandler)
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`))
