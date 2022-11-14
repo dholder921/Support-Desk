0123f1f217af97a3676d450356ba9704f7f2ad93
Darrel Holder <darrel.holder@gmail.com>
2022-11-14 11:19:15 -0500

    Added ticket reducer - in Redux toggle Log Monitor if ticket doesnt show E140

--- frontend/src/features/tickets/ticketSlice.js
+++ frontend/src/features/tickets/ticketSlice.js
@@ -9,6 +9,38 @@ const initialState = {
     isLoading: false,
     message: ''
 }
+
+export const ticketSlice = createSlice({
+    name: 'ticket',
+    initialState,
+    reducers: {
+        reset: (state) => initialState
+    },
+    extraReducers: (builder) => {
+        /*builder
+        .addCase(createTicket.pending, (state)=> {
+            state.isLoading = true
+        }).addCase(createTicket.fulfilled, (state) => {
+            state.isLoading = false
+            state.isSuccess = true
+        }).addCase(createTicket.rejected, (state, action) => {
+            state.isLoading = false
+            state.isError = true
+            state.message = action.payload
+        }).addCase(getTickets.pending, (state) => {
+            state.isLoading = true
+        }).addCase(getTickets.fulfilled, (state, action) => {
+            state.isLoading = false
+            state.isSuccess = true
+            state.tickets = action.payload
+        }).addCase(getTickets.rejected, (state, action) => {
+            state.isLoading = false
+            state.isError = true
+            state.message = action.payload
+        })*/
+    }
+})
+/*
 //create new ticket
 export const createTicket = createAsyncThunk(
     "tickets/create",
@@ -36,37 +68,8 @@ export const getTickets = createAsyncThunk(
         }
     }
 );
+*/
 
-export const ticketSlice = createSlice({
-    name: 'ticket',
-    initialState,
-    reducers: {
-        reset: (state) => initialState
-    },
-    extraReducers: (builder) => {
-builder
-.addCase(createTicket.pending, (state)=> {
-    state.isLoading = true
-}).addCase(createTicket.fulfilled, (state) => {
-    state.isLoading = false
-    state.isSuccess = true
-}).addCase(createTicket.rejected, (state, action) => {
-    state.isLoading = false
-    state.isError = true
-    state.message = action.payload
-}).addCase(getTickets.pending, (state) => {
-    state.isLoading = true
-}).addCase(getTickets.fulfilled, (state, action) => {
-    state.isLoading = false
-    state.isSuccess = true
-    state.tickets = action.payload
-}).addCase(getTickets.rejected, (state, action) => {
-    state.isLoading = false
-    state.isError = true
-    state.message = action.payload
-})
-    }
-})
 
 export const {reset} = ticketSlice.actions
 export default ticketSlice.reducer
--- frontend/src/pages/NewTicket.jsx
+++ frontend/src/pages/NewTicket.jsx
@@ -1,9 +1,94 @@
-import React from 'react'
+import { useState, useEffect } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { useNavigate } from "react-router-dom";
+import { toast } from "react-toastify";
+import { BackButton } from "../components/BackButton";
+import Spinner from "../components/Spinner";
+import { createTicket, reset } from "../features/tickets/ticketSlice";
 
 function NewTicket() {
+  const { user } = useSelector((state) => state.auth);
+  const [name] = useState(user.name);
+  const [email] = useState(user.email);
+  const [product, setProduct] = useState("");
+  const [description, setDescription] = useState("");
+
+
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+
+  /*useEffect(() => {
+    if (isError) {
+      toast.error(message);
+    }
+    if (isSuccess) {
+      dispatch(reset());
+      navigate("/ticket");
+    }
+
+    dispatch(reset());
+  }, [dispatch, isError, isSuccess, navigate, message, reset]);
+*/
+  const onSubmit = (e) => {
+    e.preventDefault();
+    //dispatch(createTicket({ product, description }));
+  };
+/*
+  if (isLoading) {
+    return Spinner;
+  }*/
   return (
-    <h1>NewTicket</h1>
-  )
+    <>
+      <BackButton url="/" />
+      <section className="heading">
+        <h1>Create New Ticket</h1>
+        <p>Please fill out the form below</p>
+      </section>
+
+      <section className="form">
+        <div className="form-group">
+          <label htmlFor="name">Customer Name</label>
+          <input type="text" className="form-control" value={name} disabled />
+        </div>
+        <div className="form-group">
+          <label htmlFor="email">Customer Email</label>
+          <input type="text" className="form-control" value={email} disabled />
+        </div>
+        <form onSubmit={onSubmit}>
+          <div className="form-group">
+            <label htmlFor="product">Product</label>
+            <select
+              name="product"
+              id="product"
+              value={product}
+              onChange={(e) => setProduct(e.target.value)}
+            >
+              <option value="iphone">iPhone</option>
+              <option value="iphone">iMac</option>
+              <option value="iphone">iPad</option>
+              <option value="iphone">MacBookPro</option>
+            </select>
+          </div>
+          <div className="form-group">
+            <label htmlFor="description">Description of Issue</label>
+            <textarea
+              name="description"
+              id="description"
+              cols="30"
+              rows="10"
+              className="form-control"
+              placeholder="Description of issue"
+              value={description}
+              onChange={(e) => setDescription(e.target.value)}
+            ></textarea>
+          </div>
+          <div className="form-group">
+            <button className="btn btn-block">Submit</button>
+          </div>
+        </form>
+      </section>
+    </>
+  );
 }
 
-export default NewTicket
+export default NewTicket;
